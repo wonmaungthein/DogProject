af7e4d2e58bd4e20c525de25ac71580d8f4215ab
Won Maung Thein <wonmaungthein@gmail.com>
2018-06-03 14:16:53 +0100

    added functions for testing

--- src/components/RandomDog.js
+++ src/components/RandomDog.js
@@ -1,20 +1,39 @@
-import React from 'react';
-import './RandomDog.css';
+import React from "react";
+import "./RandomDog.css";
+
+fetch("https://dog.ceo/api/breeds/image/random")
+  .then(data => data.json())
+  .then(randompic => {
+    randompic;
+  });
 
 class RandomDog extends React.Component {
+  saveImage = () => {
+    console.log("Save button is being clicked");
+  };
 
-	render() {
-		return (
-			<div className="RandomDog">
-				<h2 className="RandomDog-title">Random Dogs</h2>
-				<img className="RandomDog-image" src="http://via.placeholder.com/300x300" />
-				<p>
-					<button className="RandomDog-button">Save Image</button>
-					<button className="RandomDog-button">Next Dog</button>
-				</p>
-			</div>
-		)
-	}
+  getNextImage = () => {
+    console.log("Let's get another img");
+  };
+  render() {
+    return (
+      <div className="RandomDog">
+        <h2 className="RandomDog-title">Random Dogs</h2>
+        <img
+          className="RandomDog-image"
+          src="http://via.placeholder.com/300x300"
+        />
+        <p>
+          <button className="RandomDog-button" onClick={this.saveImage}>
+            Save Image
+          </button>
+          <button className="RandomDog-button" onClick={this.getNextImage}>
+            Next Dog
+          </button>
+        </p>
+      </div>
+    );
+  }
 }
 
 export default RandomDog;
