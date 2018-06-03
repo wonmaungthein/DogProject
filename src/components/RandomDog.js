5e46d3e649082ac10ccb71c11777f7a4246cc712
Won Maung Thein <wonmaungthein@gmail.com>
2018-06-03 17:04:39 +0100

    display random dog photos

--- src/components/RandomDog.js
+++ src/components/RandomDog.js
@@ -1,14 +1,23 @@
 import React from "react";
 import "./RandomDog.css";
-<<<<<<< HEAD
-
-fetch("https://dog.ceo/api/breeds/image/random")
-  .then(data => data.json())
-  .then(randompic => {
-    randompic;
-  });
 
 class RandomDog extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      randomDog: []
+    };
+  }
+
+  componentDidMount() {
+    const randomDogLink = "https://dog.ceo/api/breeds/image/random";
+    fetch(randomDogLink)
+      .then(data => data.json())
+      .then(randompic => {
+        this.setState({ randomDog: randompic.message });
+      });
+  }
+
   saveImage = () => {
     console.log("Save button is being clicked");
   };
@@ -16,29 +25,18 @@ class RandomDog extends React.Component {
   getNextImage = () => {
     console.log("Let's get another img");
   };
-=======
-class RandomDog extends React.Component {
->>>>>>> master
   render() {
     return (
       <div className="RandomDog">
         <h2 className="RandomDog-title">Random Dogs</h2>
-        <img
-          className="RandomDog-image"
-          src="http://via.placeholder.com/300x300"
-        />
+        <img className="RandomDog-image" src={this.state.randomDog} />
         <p>
-<<<<<<< HEAD
           <button className="RandomDog-button" onClick={this.saveImage}>
             Save Image
           </button>
           <button className="RandomDog-button" onClick={this.getNextImage}>
             Next Dog
           </button>
-=======
-          <button className="RandomDog-button">Save Image</button>
-          <button className="RandomDog-button">Next Dog</button>
->>>>>>> master
         </p>
       </div>
     );
