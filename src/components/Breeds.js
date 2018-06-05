5298f42be261d0028e72a8ef20c1ee42f22e23f0
Won Maung Thein <wonmaungthein@gmail.com>
2018-06-05 06:30:21 +0100

    display Beagle breed photos when clicking

--- src/components/Breeds.js
+++ src/components/Breeds.js
@@ -1,27 +1,52 @@
-import React from 'react';
-import './Breeds.css';
+import React from "react";
+import "./Breeds.css";
 
 class Breeds extends React.Component {
-	render() {
-		return (
-			<div className="Breeds">
-				<h2 className="Breeds-title">Select a Breed</h2>
-				<p>
-					<select className="Breeds-select">
-						<option>beagle</option>
-						<option>bluetick</option>
-						<option>bullterrier-staffordshire</option>
-						<option>malinois</option>
-						<option>wolfhound-irish</option>
-					</select>
-				</p>
-				<img className="Breeds-image" src="http://via.placeholder.com/300x300" />
-				<p>
-					<button className="Breeds-button">Show me more!</button>
-				</p>
-			</div>
-		)
-	}
+  constructor() {
+    super();
+    this.state = {
+      BeagleBreed: []
+    };
+  }
+
+  componentDidMount() {
+    this.getNextImage();
+  }
+
+  getNextImage = () => {
+    const BeagleLink = "https://dog.ceo/api/breed/beagle/images";
+    fetch(BeagleLink)
+      .then(data => data.json())
+      .then(dogs => {
+        const dogsArray = dogs.message;
+        this.setState({
+          BeagleBreed: dogsArray[Math.floor(Math.random() * dogsArray.length)]
+        });
+      });
+  };
+
+  render() {
+    return (
+      <div className="Breeds">
+        <h2 className="Breeds-title">Select a Breed</h2>
+        <p>
+          <select className="Breeds-select">
+            <option>beagle</option>
+            <option>bluetick</option>
+            <option>bullterrier-staffordshire</option>
+            <option>malinois</option>
+            <option>wolfhound-irish</option>
+          </select>
+        </p>
+        <img className="Breeds-image" src={this.state.BeagleBreed} />
+        <p>
+          <button className="Breeds-button" onClick={this.getNextImage}>
+            Show me more!
+          </button>
+        </p>
+      </div>
+    );
+  }
 }
 
 export default Breeds;
