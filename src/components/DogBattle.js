3a2a140a7cecf9d68b3b87a7161a59b58d3b26d7
swathi <priyaswa14@gmail.com>
2018-06-06 12:24:16 +0100

    working process

--- src/components/DogBattle.js
+++ src/components/DogBattle.js
@@ -3,13 +3,25 @@ import Dog from "./Dog.js";
 import "./DogBattle.css";
 
 class DogBattle extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      DogBattle: []
+    };
+  }
   render() {
     return (
       <div className="DogBattle">
         <h2 className="DogBattle-title">Choose the best dog</h2>
         <div className="DogBattle-images">
-          <Dog image="http://via.placeholder.com/300x300" />
-          <Dog image="http://via.placeholder.com/300x300" />
+          <Dog
+            image="http://via.placeholder.com/300x300"
+            src="http://via.placeholder.com/300x300"
+          />
+          <Dog
+            image="http://via.placeholder.com/300x300"
+            src="http://via.placeholder.com/300x300"
+          />
           <p>my dogs battle</p>
         </div>
       </div>
